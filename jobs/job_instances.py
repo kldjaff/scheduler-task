884cdf2cbab7d087b42dea1d01db41c527279c3c
Yi Hu <yi.hu@unitediot.com.cn>
2021-08-05 13:41:34 +0800

    adding scheduleing

---
+++ jobs/job_instances.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+import json
+import logging
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+logging.basicConfig(format='%(levelname)5s %(asctime)-15s #%(filename)-20s@%(funcName)-20s: %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class GenApi(object):
+    """
+    """
+    def __init__(self, method="GET", endpoint="www.baidu.com", **kwargs):
+        """
+        :param endpoint:
+        :param args:
+        :param kwargs:
+        """
+        self.end_point = endpoint
+        self.s = requests.Session()
+        self.retries = Retry(total=3, backoff_factor=0.2, status_forcelist=[500, 502, 503, 504])
+        self.s.mount('https://', HTTPAdapter(max_retries=self.retries))
+
+        self.method = method
+        self.dict_params = kwargs
+
+    def run(self):
+        logging.info(f"endpoint: {self.end_point} dict params : {self.dict_params} method: {self.method}")
+        if "payload" in self.dict_params.keys():
+            payload = self.dict_params['payload']
+        else:
+            payload = ""
+        if "headers" in self.dict_params.keys():
+            headers = self.dict_params['headers']
+        else:
+            headers = ""
+        if "params" in self.dict_params.keys():
+            params = self.dict_params['params']
+        else:
+            params = ""
+
+        response = self.s.request(self.method, self.end_point, data=payload, headers=headers, params=params)
+        logging.info(f"{response.text}")
+
+
+if __name__ == "__main__":
+    gen_api = GenApi(endpoint="", payload="test payload")
+    gen_api.run()
---
+++ start.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+import schedule
+import os
+import logging
+
+import local_settings
+from jobs import GenApi
+
+
+logging.basicConfig(format='%(levelname)5s %(asctime)-15s #%(filename)-20s@%(funcName)-20s: %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+if __name__ == "__main__":
+
+    interval = os.getenv("SCHEDULE_INTERVAL")
+    interval_unit = os.getenv("INTERVAL_UNIT")
+    logging.info(f"Scheduled Job will be running every {interval} {interval_unit}s")
+    gen_api = GenApi(
+        method=local_settings.METHOD,
+        endpoint=local_settings.ENDPOINT,
+        header=local_settings.HEADER
+    )
+
+    if interval_unit == "second":
+        schedule.every(interval).seconds.do(gen_api.run)
+    elif interval_unit == "hour":
+        schedule.every(interval).hours.do(gen_api.run)
+    elif interval_unit == "day":
+        schedule.every(interval).days.do(gen_api.run)
+    else:
+        schedule.every(1).seconds.do(gen_api.run)
+
+    while True:
+        schedule.run_pending()
