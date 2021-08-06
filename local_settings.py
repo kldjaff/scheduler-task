fa539a883eb81a661a7fe0e0e9d49c821ca26e5e
wuxiaochun <xiaochun.wu@unitediot.com.cn>
2021-08-06 10:05:59 +0800

    expose config file

---
+++ local_settings.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+ENDPOINT = "http://localhost/cts/api/v1.0/mdm/push"
+METHOD = "GET"
+HEADER = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache"
+}
+PAYLOAD = {}
+PARAMS = {}
