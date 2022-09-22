43a936aed634ba4627a8469a03bd8c1a35fbaf86
wuxiaochun <xiaochun.wu@unitediot.com.cn>
2022-09-22 18:55:07 +0800

    support payload in json format

--- local_settings.py
+++ local_settings.py
@@ -1,18 +1,6 @@
 # coding=utf-8
 TASKS = [
     {
-        'INTERVAL_UNIT': 'minute',
-        'SCHEDULE_INTERVAL': 2,
-        'ENDPOINT' : "http://localhost:5000/hello",
-        'METHOD' : "GET",
-        'HEADER' : {
-            'Content-Type': "application/json",
-            'cache-control': "no-cache"
-        },
-        'PAYLOAD' : {},
-        'PARAMS' : {}
-    },
-    {
         'title': '导入format文件',
         'INTERVAL_UNIT': 'hour',
         'SCHEDULE_INTERVAL': 1,
@@ -22,8 +10,7 @@ TASKS = [
             'Content-Type': "application/json",
             'cache-control': "no-cache"
         },
-        'PAYLOAD' : {},
-        'PARAMS' : {}
+        'PAYLOAD_JSON' : {}
     },
     {
         'title': '导入zip文件',
@@ -35,46 +22,79 @@ TASKS = [
             'Content-Type': "application/json",
             'cache-control': "no-cache"
         },
-        'PAYLOAD': {},
-        'PARAMS': {}
+        'PAYLOAD_JSON': {}
+    },
+    {
+        'title': '导入dat文件',
+        'INTERVAL_UNIT': 'hour',
+        'SCHEDULE_INTERVAL': 1,
+        'ENDPOINT': "http://localhost:5001/tds/api/v1.0/import/dat",
+        'METHOD': "GET",
+        'HEADER': {
+            'Content-Type': "application/json",
+            'cache-control': "no-cache"
+        },
+        'PAYLOAD_JSON': {}
     },
     {
         'title': '向五室推送数据',
-        'INTERVAL_UNIT': 'hour',
-        'SCHEDULE_INTERVAL': 12,
+        'INTERVAL_UNIT': 'minute',
+        'SCHEDULE_INTERVAL': 1,
         'ENDPOINT': "http://localhost:5000/cts/api/v1.0/mdm/push",
         'METHOD': "GET",
         'HEADER': {
-            'Content-Type': "application/json",
+            'Content-Type': "application/x-www-form-urlencoded",
             'cache-control': "no-cache"
         },
-        'PAYLOAD': {},
-        'PARAMS': {}
+        'PARAMS': { 'limit': 5 }
     },
     {
         'title': '下载五室的文件',
-        'INTERVAL_UNIT': 'hour',
-        'SCHEDULE_INTERVAL': 6,
-        'ENDPOINT': "http://localhost:5000/cts/api/v1.0/mdm/push/ex1/download",
+        'INTERVAL_UNIT': 'day',
+        'SCHEDULE_INTERVAL': 1,
+        'ENDPOINT': "http://localhost:5000/cts/api/v1.0/mdm/ex1/download",
         'METHOD': "GET",
         'HEADER': {
             'Content-Type': "application/json",
             'cache-control': "no-cache"
-        },
-        'PAYLOAD': {},
-        'PARAMS': {}
+        }
     },
     {
-        'title': '下载八室的数据',
-        'INTERVAL_UNIT': 'hour',
-        'SCHEDULE_INTERVAL': 6,
+        'title': '八室数据同步 - 售后数据和成功子样',
+        'INTERVAL_UNIT': 'minute',
+        'SCHEDULE_INTERVAL': 1,
         'ENDPOINT': "http://localhost:5000/cts/api/v1.0/r8mro/sync",
+        'METHOD': "POST",
+        'HEADER': {
+            'Content-Type': "application/json",
+            'cache-control': "no-cache"
+        },
+        'PAYLOAD_JSON': {"steps": ["sample", "postsale"]}
+    },
+    {
+        'title': '八室数据同步 - 产品目录',
+        'INTERVAL_UNIT': 'minute',
+        'SCHEDULE_INTERVAL': 2,
+        'ENDPOINT': "http://localhost:5000/cts/api/v1.0/r8mro/sync",
+        'METHOD': "POST",
+        'HEADER': {
+            'Content-Type': "application/json",
+            'cache-control': "no-cache"
+        },
+        'PAYLOAD_JSON': {'steps': ['product']}
+    },
+    {
+        'title': 'cts空间清理',
+        'INTERVAL_UNIT': 'hour',
+        'SCHEDULE_INTERVAL': 12,
+        'ENDPOINT': "http://localhost:5000/hello",
         'METHOD': "GET",
         'HEADER': {
             'Content-Type': "application/json",
             'cache-control': "no-cache"
         },
-        'PAYLOAD': {'steps': ['product', 'sample', 'postsale']},
+        'PAYLOAD_JSON': {},
+        'PAYLOAD': {},
         'PARAMS': {}
     }
 ]
